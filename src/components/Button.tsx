e33be0f5bdd2f129736d425d71e8e1759e242ada
Mir Sameer <mir.sameer@example.com>
2024-05-09 13:46:35 -0400

    added modal for login form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,12 @@
 import { twMerge } from 'tailwind-merge'
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    handleButton?: () => void
+}
 
-const Button = ({ children, className }: ButtonProps) => {
+const Button = ({ children, className, handleButton }: ButtonProps) => {
     return (
-        <button className={twMerge("text-white rounded-3xl text-xs px-2.5 py-1 bg-gray flex justify-center items-center gap-2 ring-1 ring-lite md:text-md lg:py-2 lg:px-4", className)}>
+        <button onClick={handleButton} className={twMerge("text-white rounded-3xl text-xs px-2.5 py-1 bg-gray flex justify-center items-center gap-2 ring-1 ring-lite md:text-md lg:py-2 lg:px-4", className)}>
             {children}
         </button>
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,41 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import Button from "./Button"
 import { FiBell, FiChevronDown, FiLogIn } from 'react-icons/fi'
 import { AuthContext } from "../context/AuthContext"
+import Login from "./Login"
 
 const Header = () => {
 
   const auth = useContext(AuthContext)
+  const [showLogin, setShowLogin] = useState(false)
+
+  const handleClose = () => {
+    setShowLogin(showLogin => !showLogin)
+  }
 
   return (
-    <header className="py-2 px-4 flex justify-between">
-      <h1 className="text-3xl lg:text-5xl inline-block font-bold from-primary via-secondary to-primary bg-gradient-to-r bg-clip-text text-transparent">Linkly</h1>
+    <>
+      <header className="py-2 px-4 flex justify-between">
+        <h1 className="text-3xl lg:text-5xl inline-block font-bold from-primary via-secondary to-primary bg-gradient-to-r bg-clip-text text-transparent">Linkly</h1>
 
+        {
+          auth ?
+            <div className="flex justify-center items-center gap-4">
+              {/* @TODO: Add a dropdown menu here */}
+              <Button className="rounded-full"> {auth} <FiChevronDown /> </Button>
+              <Button className="bg-primary ring-primary"> <FiBell /> </Button>
+            </div>
+            :
+            <div className="flex justify-center items-center gap-4">
+              <Button handleButton={() => setShowLogin(true)}> <span className="hidden md:block"> Sign In </span>  <FiLogIn /> </Button>
+              <Button className="bg-primary ring-primary">Sign Up </Button>
+            </div>
+        }
+      </header>
       {
-        auth ?
-          <div className="flex justify-center items-center gap-4">
-            {/* @TODO: Add a dropdown menu here */}
-            <Button className="rounded-full"> {auth} <FiChevronDown /> </Button>
-            <Button className="bg-primary ring-primary"> <FiBell /> </Button>
-          </div>
-          :
-          <div className="flex justify-center items-center gap-4">
-            <Button> <span className="hidden md:block"> Sign In </span>  <FiLogIn /> </Button>
-            <Button className="bg-primary ring-primary">Sign Up </Button>
-          </div>
+        showLogin ? <Login handleClose={handleClose} /> : null
       }
-    </header>
+    </>
   )
 }
 
